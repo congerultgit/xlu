311c4b44af44b87aff68a1847fb9926b1b77527e
lixing <lixing@51cto.com>
2016-09-09 18:01:55 +0800

    test2

--- lib/di/DiContainer.php
+++ lib/di/DiContainer.php
@@ -7,17 +7,18 @@ class DiContainer extends  BaseComponent{
 	//容器对象实例
 	private $singletons = [];
 	//容器对应定义
-	private $definitions = [];		
+	private $definitions = [];
+		
 	
 	public function get($type,$param=array(),$array=array()){
 		
+		//生成唯一的key
+		$key = $type.base64_encode(json_encode($param)).base64_encode(json_encode($array));
+		
 		$tmp_obj = null;
 		if(is_string($type)){
-			if(is_set($this->singletons[$type])){
-				return $this->singletons[$type];
-			}
 			$tmp_obj = $this->set($type,$param,$array);
-			$this->singletons[$type] = $tmp_obje;
+			$this->singletons[$key] = $tmp_obj;
 			return $tmp_obj;
 			
 		}
@@ -26,7 +27,7 @@ class DiContainer extends  BaseComponent{
 				$type_class = $type['class'];
 				unset($type['class']);
 				$tmp_obj = $this->set($type,$param,$array);
-				$this->singletons[$type] = $tmp_obje;
+				$this->singletons[$key] = $tmp_obj;
 				return $tmp_obj;				
 			}			
 		}
@@ -37,10 +38,22 @@ class DiContainer extends  BaseComponent{
 	
 	
 	public function set($type,$param=array(),$array=array()){
+
+		$key = $type.base64_encode(json_encode($param)).base64_encode(json_encode($array));
+	
+		echo $type;
+		if(class_exists($type) == FALSE){
+			$this->singletons[$key] = $param;
+			return $param;
+		}
 		
 		$class_name = $type;
 		$constructor_param = $param;
-		$public_param = $array();
+		$public_param = $array;
+		
+		$tmp_object = DiInstance::createObject($type,$param,$array);
+		
+		return $tmp_object;
 		
 	}	
 	
--- lib/di/DiInstance.php
+++ lib/di/DiInstance.php
@@ -1,10 +1,76 @@
 <?php
 namespace xlu\lib\di;
 use xlu\lib\base\BaseComponent;
+use ReflectionClass;
 //对象容器
 class DiInstance extends  BaseComponent{
+	
+	
+	public static  $selfClass;
+	
+	public $definitions = array();
+	
+	
 
+    private $_reflections = [];
 
+    private $_dependencies = [];
+	
+	public static function createObject($class='',$param=array(),$array=array()){
+		
+		$self_class = '';
+		if(empty(self::$selfClass)){
+			self::$selfClass = new self();
+			$self_class = self::$selfClass;
+		}else{
+			$self_class = self::$selfClass;
+		}
+		$object = $self_class->build($class='',$param=array(),$array=array());
+		
+		
+		
+		
+		
+	}
+	
+	
+	private function build($class='',$param=array(),$array=array()){
+		
+		
+		list ($reflection, $dependencies) = $this->getDependencies($class);
+		var_dump($refection);
+		var_dump($dependencies);
+		exit;
+		
+		
+	} 
+	
+	
+
+    protected function getDependencies($class)
+    {
+
+        $dependencies = [];
+		echo $class;
+        $reflection = new ReflectionClass($class);
+		exit;
+        $constructor = $reflection->getConstructor();
+        if ($constructor !== null) {
+            foreach ($constructor->getParameters() as $param) {
+                if ($param->isDefaultValueAvailable()) {
+                    $dependencies[] = $param->getDefaultValue();
+                } else {
+                    $c = $param->getClass();
+                    $dependencies[] = Instance::of($c === null ? null : $c->getName());
+                }
+            }
+        }
+
+        $this->_reflections[$class] = $reflection;
+        $this->_dependencies[$class] = $dependencies;
+
+        return [$reflection, $dependencies];
+    }
 	
 }
 
